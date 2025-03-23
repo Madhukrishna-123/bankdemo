29fcea8450adf886c34049df2c933cc4c02c6406
Madhu Krishna Nakka <krishnamadhu0311@gmail.com>
2025-03-23 18:43:57 +0530

    Fixing forgot password

--- src/main/java/com/example/BankDemo/service/UserService.java
+++ src/main/java/com/example/BankDemo/service/UserService.java
@@ -1,7 +1,5 @@
 package com.example.BankDemo.service;
 
-import java.util.List;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +17,6 @@ public class UserService {
     } 
 
     public void save(User user) {
-        // Ensure password is hashed before saving
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
